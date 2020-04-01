1d9a92bef358a2f49050b8228ffdc1084c359998
Epiclygend <alwan.nuha7@gmail.com>
2020-04-01 10:19:45 +0700

    improve structure code of new Table function

--- Epic-JS-1.0.js
+++ Epic-JS-1.0.js
@@ -9,7 +9,7 @@ if (showBenchmark) { console.time('Benchmark: ') }
 
 // helper
 function streplace(elmt,replaced,replace) {
-	let ori = line.textContent.match(re.between("\\[{1}","\\]{1}"));
+	let ori = line.textContent.match(re.between('\\[{1}','\\]{1}'));
 	if (ori) { for (x of ori) elmt.innerHTML = elmt.textContent.replace(replaced,replace) }
 }
 
@@ -23,7 +23,7 @@ function createNewElement(tag,data,appendTo,...classes) {
 }
 function toggleClass(...arrayElmAndClasstoToggle) {
 	// example
-	//  var = new toggle([form, ["disappear"]], [document.getElementById("resp"), ["disappear"]])
+	//  var = new toggle([form, ['disappear']], [document.getElementById('resp'), ['disappear']])
 	// 	@arg is array. first is node. second is array of class want to toggle
 	this.dat = arrayElmAndClasstoToggle
 	this.off = () => this.dat.forEach(elm => elm[0].classList.remove(elm[1]))
@@ -39,30 +39,31 @@ function toggleClass(...arrayElmAndClasstoToggle) {
 }
 
 // DOM manipulation
-function Table(data, tbl = document.createElement("TABLE"), options = {}) {
+function Table(data, tbl = document.createElement('TABLE'), options = {}) {
 	this.data = data,
 	this.options = options,
 	this.elm = tbl,
 	this.elmRow = this.elm.rows;
 
-	if ("class" in this.options) this.elm.classList.add(...this.options.class)
-	if ("title" in this.options) this.elm.createCaption().textContent = this.options.title
+	if ('class' in this.options) this.elm.classList.add(...this.options.class)
+	if ('title' in this.options) this.elm.createCaption().textContent = this.options.title
 
 	this.create = () => {
-		let heads = body = []
-		// Create Head
-		this.elm.createTHead()
-		this.data.forEach(row => heads = heads.concat(Object.keys(row)))
+		// Filter Key to inserts in Head
+		let heads = this.data.reduce((heads, row) => [...heads, ...Object.keys(row)], [])
 		heads = [...new Set(heads)]
+		// Make undefined value from row to be null cell
+		let body = this.data.map(row => heads.map(col => col in row ? row[col] : null))
+		
+		this.elm.createTHead()
+		this.elm.createTBody()
 		rw = this.elm.tHead.insertRow()
 		heads.forEach(head => createNewElement('TH',head,rw))
-		// Create Body
-		this.elm.createTBody()
-		this.data.forEach(row => body.push(heads.slice().map(col => { return col in row ? row[col] : null }))) // Make undefined key to be null cell
 		body.forEach(row => this.addRow(row))
 	}
 	this.addRow = (arrOfCell,toPart = this.elm.tBodies[0]) => {
 		let row = toPart.insertRow()
+		if ('eachRow' in this.options) this.options.eachRow(row, arrOfCell)
 		arrOfCell.forEach(cell => (row.insertCell()).innerHTML = cell)
 	}
 }
@@ -72,39 +73,39 @@ function loader(appendTo = document.body, loaderElement) {
 	this.appendTo = appendTo
 	if (this.loaderElement) this.loaderElement = loaderElement
 	else {
-		this.loaderElement = document.createElement("DIV")
-		this.loaderElement.classList.add("loader-container")
-		createNewElement("DIV", null, createNewElement("DIV", null, this.loaderElement, "line-loader"), "line");
+		this.loaderElement = document.createElement('DIV')
+		this.loaderElement.classList.add('loader-container')
+		createNewElement('DIV', null, createNewElement('DIV', null, this.loaderElement, 'line-loader'), 'line');
 	}
 	this.isLoad = () => this.appendTo.appendChild(this.loaderElement)
 	this.isLoaded = () => this.loaderElement.remove()
 }
 function toolTips() {
-	document.querySelectorAll("[data-tooltip]").forEach(elmt => {
+	document.querySelectorAll('[data-tooltip]').forEach(elmt => {
 		let tElmt;
-		elmt.addEventListener("mouseenter", e => {
-			tElmt = createNewElement("DIV",elmt.dataset.tooltip, document.body, "tooltip");
+		elmt.addEventListener('mouseenter', e => {
+			tElmt = createNewElement('DIV',elmt.dataset.tooltip, document.body, 'tooltip');
 			tElmt.style.left = `${e.clientX - tElmt.getBoundingClientRect().width / 3}px`;
 			currPos.y = e.clientY + 10;
-			((currPos.y + tElmt.getBoundingClientRect().height) >= window.innerHeight) ? tElmt.style.top = (currPos.y - (elmt.getBoundingClientRect().height * 2.1))+"px" : tElmt.style.top = (currPos.y)+"px"
+			((currPos.y + tElmt.getBoundingClientRect().height) >= window.innerHeight) ? tElmt.style.top = (currPos.y - (elmt.getBoundingClientRect().height * 2.1))+'px' : tElmt.style.top = (currPos.y)+'px'
 		})
-		elmt.addEventListener("mouseleave", () => tElmt.remove());
+		elmt.addEventListener('mouseleave', () => tElmt.remove());
 	})
 }
 function modal() {
-	const modElmt = document.querySelector("div.modals");
+	const modElmt = document.querySelector('div.modals');
 	
-	this.close = () => { modElmt.classList.remove("open") }
-	this.open = () => { modElmt.classList.add("open") }
+	this.close = () => { modElmt.classList.remove('open') }
+	this.open = () => { modElmt.classList.add('open') }
 }
 function code() {
-	const codeBlock = [].slice.call(document.querySelectorAll("code"));
+	const codeBlock = [].slice.call(document.querySelectorAll('code'));
 	codeBlock.forEach((block) => {
 		const ctn = block.textContent, re = /(<((\/)?([a-z]*)(\s*(([^>]*))+)*)?>)/gi;
-		if (block.querySelectorAll("*").length === 0) {
+		if (block.querySelectorAll('*').length === 0) {
 			const sep = ctn.split(/\n/g).filter(ssep => /\S/g.test(ssep)); block.textContent = null;
-			if (sep.length > 0) { sep.forEach(ssep => createNewElement("DIV",ssep,block));
-			} else { createNewElement("DIV",ctn,block) }
+			if (sep.length > 0) { sep.forEach(ssep => createNewElement('DIV',ssep,block));
+			} else { createNewElement('DIV',ctn,block) }
 		}
 	});
 }
@@ -116,14 +117,14 @@ function code() {
 // 	this.ctn = new Object(this.elmt.querySelector('tbody').children);
 // 	const dElmt = document.querySelector('ul.tblEdit-dialog');
 // 	Object.values(this.ctn).forEach(function (tr) {
-// 		tr.addEventListener("click", function (e) {
+// 		tr.addEventListener('click', function (e) {
 // 			currPos.y = tr.getBoundingClientRect().y;
 // 			currPos.x = e.clientX;
-// 			dElmt.style.display = "unset";
-// 			dElmt.style.top = (currPos.y - (tr.getBoundingClientRect().height))+"px";
-// 			dElmt.style.left = (currPos.x - (dElmt.getBoundingClientRect().width / 2.5))+"px";
+// 			dElmt.style.display = 'unset';
+// 			dElmt.style.top = (currPos.y - (tr.getBoundingClientRect().height))+'px';
+// 			dElmt.style.left = (currPos.x - (dElmt.getBoundingClientRect().width / 2.5))+'px';
 // 			setTimeout(function () {
-// 				dElmt.style.display = "none";
+// 				dElmt.style.display = 'none';
 // 			}, 5000);
 // 		});
 // 	});
